7c547b3793734d716d47fc8c069e4ae75afa94cb
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2023-05-12 15:58:15 +0900

    Update cumulative_incidence_plot.R

--- Results/cumulative_incidence_plot.R
+++ Results/cumulative_incidence_plot.R
@@ -17,4 +17,4 @@ mt$est <- as.data.frame(binconf(mt$cum,(18765+mt$cum),method='exact'))
 ## Calculate incidence and confidence interval per risk group. and combine them.
 
 
-ggplot(mt,aes(x=onset,y=est$PointEst,color=per10,fill=per10))+geom_step(size=1.5)+geom_ribbon(aes(ymin=lower$Lower,ymax = upper$Upper),alpha=.1,linetype=0)+theme_bw()+ylab("Incidence")+xlab("Onset age")+ylim(0,0.01)+theme(legend.position='none')
+ggplot(mt,aes(x=onset,y=est$PointEst,color=per10,fill=per10))+geom_step(size=1.5)+geom_ribbon(aes(ymin= est$Lower,ymax = est$Upper),alpha=.1,linetype=0)+theme_bw()+ylab("Incidence")+xlab("Onset age")+ylim(0,0.01)+theme(legend.position='none')+scale_color_manual(values= c("#00BFC4","#F8766D"))+scale_fill_manual(values= c("#00BFC4","#F8766D"))
